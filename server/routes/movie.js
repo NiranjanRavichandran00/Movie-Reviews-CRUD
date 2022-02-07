ab041c85d06573ab5b2768c634ecd61658c12843
NiranjanRavichandran00 <nxr170005@utdallas.edu>
2022-02-06 20:40:28 -0600

    Added Search by name and Sort by name

--- server/routes/movie.js
+++ server/routes/movie.js
@@ -5,8 +5,9 @@ router.route('/insert').post( async (req,res) => {
     const movieName = req.body.movieName;
     const watchTime = req.body.lastWatched;
     const rate = req.body.rating;
+    const review = req.body.review;
 
-    const movie = new MovieModel({movieName: movieName, watchDate: watchTime, rating: rate});
+    const movie = new MovieModel({movieName: movieName, watchDate: watchTime, rating: rate, review: review});
     try {
         await movie.save();
         res.send("inserted data");  
@@ -20,6 +21,28 @@ router.route('/read').get( async (req,res) => {
         if(err) res.send(err);
         res.send(result);
     })
+});
+
+router.route('/update').put( async (req,res) => {
+    const newName = req.body.newName;
+    const id = req.body.id;
+
+    try {
+        await MovieModel.findById(id, (err, updatedMovie) => {
+            updatedMovie.movieName = newName;
+            updatedMovie.save();
+            res.send("Updated data");
+        });
+    } catch(err) {
+        console.log(err);
+    }
+}); 
+
+router.route('/delete/:id').delete( async (req,res) => {
+    const id = req.params.id;   
+    res.send(id);
+    await MovieModel.findByIdAndRemove(id).exec();
+    res.send("Deleted");
 });
 
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import moment from 'moment'
 import axios from 'axios'
 import { Rating } from 'react-simple-star-rating'
 import DatePicker from 'react-date-picker'
@@ -6,9 +7,14 @@ import DatePicker from 'react-date-picker'
 function App() {
 
   const [movieName, setMovieName] = useState('');
+  const [newName, setNewName] = useState('');
+  const [movieReview, setMovieReview] = useState('');
   const [rating, setRating] = useState(0);
+  const [changeState, setChangeState] = useState(false);
   const [date, setDate] = useState(new Date());
   const [list, setList] = useState([]);
+  const [search, setSearch] = useState('');
+  const [order, setOrder] = useState('ASC');
 
   const handleRating = (rate) => {
     setRating(rate)
@@ -19,15 +25,56 @@ function App() {
       .then((response) => {
         setList(response.data);
       })
-  }, []);
+  }, [changeState]);
 
   const addReview = () => {
-    console.log(date);
     axios.post('http://localhost:3001/movie/insert', {
       movieName: movieName,
       lastWatched: date,
-      rating: rating})
-      .then( res => console.log(res.data));
+      rating: rating,
+      review: movieReview})
+      .then( res => {
+        setChangeState(!changeState);
+        console.log(res.data)});
+    
+  }
+
+  const updateName = (id) => {
+    axios.put('http://localhost:3001/movie/update', {
+      id: id, 
+      newName: newName
+    })
+    .then ( res => {
+      setChangeState(!changeState);
+    });
+  }
+  
+  const deleteReview = (id) =>  {
+      axios.delete(`http://localhost:3001/movie/delete/${id}`)
+      .then ( res => {
+        setChangeState(!changeState);
+      });; 
+  }
+
+  const sorting = (col) => {
+      if(order === "ASC")
+      {
+        const sorted = [...list].sort((a,b) => 
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+        );
+        console.log(sorted);  
+        setList(sorted);
+        setOrder('DSC')
+      }
+
+      if(order === "DSC")
+      {
+        const sorted = [...list].sort((a,b) => 
+          a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+        );
+        setList(sorted);
+        setOrder('ASC')
+      }
   }
 
   return (
@@ -35,6 +82,7 @@ function App() {
       <h1>Movie Reviews </h1>
       <label>Movie Name</label>
       <input type = "text" onChange={(e) => {setMovieName(e.target.value)}}></input>
+      <input type = "text" placeholder='Review' onChange={(e) => {setMovieReview(e.target.value)}}></input>
       <label>Times Watched {movieName} </label>
       <DatePicker 
         onChange={setDate}
@@ -48,15 +96,30 @@ function App() {
         transition={true}
       />
       <button onClick={addReview}>Add to reviews</button>
-      {list.map((movie, key) => (
+      <button onClick={() => sorting('movieName')}>Sort by name</button>
+      <input type = "text" placeholder='Search...' onChange={(e) => {setSearch(e.target.value)}}/>
+      {list.filter((movie) => {
+        if (search === "")
+        {
+            return movie;
+        } 
+        else if(movie.movieName.toLowerCase().includes(search.toLowerCase()))
+        {
+            return movie;
+        }
+      }).map((movie, key) => (
           <div className = "item" key={key}>
              <h1>{movie.movieName}</h1>
-             <h1>{movie.watchDate.getDate}</h1>
+             <p>{movie.review}</p>
              <Rating 
               ratingValue={movie.rating}
               allowHalfIcon={true}
               readonly={true}
             />
+             <h1>{moment(movie.watchDate).format("MM/DD/YY")}</h1>
+            <input type = "text" placeHolder = "Change Name"  onChange={(e) => {setNewName(e.target.value)}}/> 
+            <button onClick={() => updateName(movie._id)}>Update</button>
+            <button onClick={() => deleteReview(movie._id)}>Delete</button>
           </div>
       ))}
     </div>
